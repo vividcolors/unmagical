3094650787fc5754b09f1542fb59e68704f9fb75
vc-tyam <tyam@vividcolors.co.jp>
2021-10-21 09:25:34 +0900

    add demo2 (stucked intermid)

---
+++ demo/demo2/src.js
@@ -0,0 +1,120 @@
+import {h, API, start, Textbox, Listbox, Radio, Checkbox, UpdateButton, SettleButton, Field, Dialog, Notification} from '../../bindings/tailwind'
+
+const master = {
+  sex: ['男', '女'], 
+  job: ['給与所得者', '会社経営', '個人事業主', '主婦', '学生']
+}
+
+const schema = {
+  type: 'object', 
+  properties: {
+    name: {
+      type: 'object', 
+      properties: {
+        firstName: {type:'string', minLength:1}, 
+        lastName: {type:'string', minLength:1}
+      }
+    }, 
+    email: {
+      type: 'object', 
+      properties: {
+        firstTime: {type:'string', pattern:'^([a-zA-Z0-9])+([a-zA-Z0-9\._-])*@([a-zA-Z0-9_-])+([a-zA-Z0-9\._-]+)+$'}, 
+        secondTime: {type:'string', minLength:1}
+      }
+    }, 
+    sex: {type:'string', enum:master.sex}, 
+    address: {
+      type: 'object', 
+      properties: {
+        zip: {type:'string', pattern:'^[0-9]{3}-?[0-9]{4}$'}, 
+        pref: {type:'string', minLength:1}, 
+        city: {type:'string', minLength:1}, 
+        street: {type:'string', minLength:1}, 
+        bld: {type:'string'}
+      }
+    }, 
+    job: {type:'string', enum:master.job}
+  }
+}
+
+const data = {
+  name: {firstName:'', lastName:''}, 
+  email: {firstTime:'', secondTime:''}, 
+  sex: '', 
+  address: {zip:'', pref:'', city:'', street:'', bld:''}, 
+  job:''
+}
+
+const foldValidity = (path, env) => {
+  const slot0 = API.getSlot(path, env)
+  const obj = slot0['@value']
+  const slot1 = Object.keys(obj).reduce((cur, p) => {
+    const cslot = obj[p]
+    if (cur.touched && cur.invalid) return cur
+    if (cur.touched != cslot.touched && cur.invalid != cslot.invalid) {
+      return {
+        ...cur, 
+        touched: cslot.touched, 
+        invalid: cslot.invalid, 
+        message: cslot.message
+      }
+    } else {
+      return cur
+    }
+  }, slot0)
+  return API.setSlot(path, slot1, env)
+}
+
+const evolve = (path, env) => {
+  env = foldValidity('/name', env)
+
+  const emails = API.extract('/email', env)
+  const eslot = API.getSlot('/email/secondTime', env)
+  if (eslot.touched && !eslot.invalid && emails.firstTime != emails.secondTime) {
+    env = API.setSlot('/email/secondTime', {...eslot, invalid:true, message:'二度入力してね'}, env)
+  }
+
+  env = foldValidity('/email', env)
+
+  env = foldValidity('/address', env)
+
+  return env
+}
+
+const view = (env) => {
+  return (
+    <div>
+      <Field title="名前" path="/name" env={env}>
+        <Textbox mg-path="/name/firstName" />
+        <Textbox mg-path="/name/lastName" />
+      </Field>
+      <Field title="メールアドレス" path="/email" env={env}>
+        <Textbox mg-path="/email/firstTime" />
+        <Textbox mg-path="/email/secondTime" />
+      </Field>
+      <Field title="生別" path="/sex" env={env}>
+        {master.sex.map(s => (
+          <Radio mg-path="/sex" name="sex" value={s} label={s} />
+        ))}
+      </Field>
+      <Field title="住所" path="/address" env={env}>
+        <Textbox mg-path="/address/zip" />
+        <Textbox mg-path="/address/pref" />
+        <Textbox mg-path="/address/city" />
+        <Textbox mg-path="/address/street" />
+        <Textbox mg-path="/address/bld" />
+      </Field>
+      <Field title="職業" path="/job" env={env}>
+        <Listbox mg-path="/job">
+          <option disabled value="">選んでください</option>
+          {master.job.map(j => (
+            <option value={j}>{j}</option>
+          ))}
+        </Listbox>
+      </Field>
+    </div>
+  )
+}
+
+const containerEl = document.getElementById('app')
+start({data, schema, view, containerEl, evolve})
